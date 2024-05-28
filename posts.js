113dcaf79edb3fc7538cccc3aee81554bf28902d
user <wainainalinahm@gmail.com>
2024-05-28 14:32:01 +0300

    first commit

---
+++ posts.js
@@ -0,0 +1,317 @@
+module.exports = [
+  {
+    "_id": "6655b7cbe2dca20a96399025",
+    "index": 0,
+    "guid": "aa4edd94-09ff-465a-9f33-617ef31a80f6",
+    "isActive": false,
+    "balance": "$2,843.11",
+    "picture": "http://placehold.it/32x32",
+    "age": 24,
+    "eyeColor": "blue",
+    "name": "Mclaughlin Edwards",
+    "gender": "male",
+    "company": "PLAYCE",
+    "email": "mclaughlinedwards@playce.com",
+    "phone": "+1 (830) 491-2977",
+    "address": "753 Cove Lane, Needmore, Ohio, 188",
+    "about": "Reprehenderit id aliqua enim qui aute commodo ex ad officia aliquip et anim dolore aliquip. Ad duis nisi minim ipsum ea tempor laborum culpa sit officia occaecat aliqua irure. Officia laboris ut mollit adipisicing do laborum. Voluptate non ex officia amet dolore ea nulla do nostrud. Quis commodo ut consectetur veniam. Consequat eiusmod fugiat ut pariatur enim laboris sunt reprehenderit pariatur aliqua voluptate quis.\r\n",
+    "registered": "2016-11-03T11:24:50 -03:00",
+    "latitude": -36.020569,
+    "longitude": -127.539161,
+    "tags": [
+      "duis",
+      "sit",
+      "ea",
+      "magna",
+      "consequat",
+      "irure",
+      "magna"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Murray Hess"
+      },
+      {
+        "id": 1,
+        "name": "Allie Barron"
+      },
+      {
+        "id": 2,
+        "name": "Charmaine Thornton"
+      }
+    ],
+    "greeting": "Hello, Mclaughlin Edwards! You have 9 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "6655b7cbd69bb6929e50b9f6",
+    "index": 1,
+    "guid": "ddacc224-0ebb-4a93-aa42-193761552e95",
+    "isActive": true,
+    "balance": "$2,086.54",
+    "picture": "http://placehold.it/32x32",
+    "age": 39,
+    "eyeColor": "blue",
+    "name": "Mitzi Woods",
+    "gender": "female",
+    "company": "AQUOAVO",
+    "email": "mitziwoods@aquoavo.com",
+    "phone": "+1 (818) 474-2299",
+    "address": "665 Caton Avenue, Camptown, District Of Columbia, 5747",
+    "about": "Esse aliqua in duis sit exercitation nisi. Id reprehenderit deserunt nisi elit ex anim quis enim duis enim elit. Elit duis reprehenderit proident consectetur sint incididunt laborum labore. Dolore labore occaecat nulla non anim laborum officia ut ipsum excepteur.\r\n",
+    "registered": "2021-08-12T11:54:43 -03:00",
+    "latitude": 64.804146,
+    "longitude": 174.243577,
+    "tags": [
+      "sunt",
+      "nisi",
+      "commodo",
+      "do",
+      "excepteur",
+      "veniam",
+      "deserunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Leah Norton"
+      },
+      {
+        "id": 1,
+        "name": "Latasha Mcpherson"
+      },
+      {
+        "id": 2,
+        "name": "Vivian Britt"
+      }
+    ],
+    "greeting": "Hello, Mitzi Woods! You have 1 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "6655b7cbdec31444b15da663",
+    "index": 2,
+    "guid": "4bfbd072-3c64-4f1d-8b26-7ee9b03d9566",
+    "isActive": false,
+    "balance": "$2,454.36",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "blue",
+    "name": "Gutierrez Peterson",
+    "gender": "male",
+    "company": "ZENTRY",
+    "email": "gutierrezpeterson@zentry.com",
+    "phone": "+1 (853) 529-3795",
+    "address": "135 Middagh Street, Kapowsin, Guam, 8855",
+    "about": "Non et eu nisi est do sunt exercitation enim nostrud laborum. Laboris est sint ullamco eiusmod nisi irure pariatur esse ipsum ut deserunt cillum proident. Do cupidatat irure magna id sunt commodo do non Lorem tempor adipisicing enim.\r\n",
+    "registered": "2020-08-26T06:04:27 -03:00",
+    "latitude": -75.267767,
+    "longitude": 133.682602,
+    "tags": [
+      "Lorem",
+      "cillum",
+      "exercitation",
+      "occaecat",
+      "veniam",
+      "consequat",
+      "qui"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Winnie Glass"
+      },
+      {
+        "id": 1,
+        "name": "Callahan Mccarthy"
+      },
+      {
+        "id": 2,
+        "name": "Bonita Neal"
+      }
+    ],
+    "greeting": "Hello, Gutierrez Peterson! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "6655b7cb4eaa1b4748b1216a",
+    "index": 3,
+    "guid": "1eef499b-7bfc-40e2-ba7c-e4ca21ccec3f",
+    "isActive": true,
+    "balance": "$3,942.13",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "green",
+    "name": "Eve Pope",
+    "gender": "female",
+    "company": "BEADZZA",
+    "email": "evepope@beadzza.com",
+    "phone": "+1 (944) 522-2362",
+    "address": "611 Lake Avenue, Romeville, North Carolina, 2985",
+    "about": "Cupidatat consectetur laborum labore consectetur culpa elit excepteur laborum nulla laboris reprehenderit eu. Eiusmod ipsum eiusmod culpa non culpa duis elit consequat anim commodo aliqua tempor aliquip ipsum. Aliqua et occaecat ad eu ullamco laboris proident. Ea eiusmod id nulla consectetur dolore. Fugiat officia nulla aute occaecat est magna velit magna adipisicing tempor deserunt.\r\n",
+    "registered": "2020-10-29T04:08:27 -03:00",
+    "latitude": 55.526198,
+    "longitude": 128.615815,
+    "tags": [
+      "cupidatat",
+      "ut",
+      "deserunt",
+      "nostrud",
+      "aute",
+      "nostrud",
+      "pariatur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Greene Robbins"
+      },
+      {
+        "id": 1,
+        "name": "Betty Franklin"
+      },
+      {
+        "id": 2,
+        "name": "Haley Lindsay"
+      }
+    ],
+    "greeting": "Hello, Eve Pope! You have 4 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "6655b7cb2c2409e80a90ded9",
+    "index": 4,
+    "guid": "103101d1-b7a1-42ab-99ac-d6a94bd7bc7a",
+    "isActive": true,
+    "balance": "$2,840.57",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": "Teresa Hebert",
+    "gender": "female",
+    "company": "QUANTASIS",
+    "email": "teresahebert@quantasis.com",
+    "phone": "+1 (924) 527-2763",
+    "address": "452 Ashland Place, Riviera, Alaska, 5654",
+    "about": "Labore dolore quis cupidatat mollit. Mollit id ullamco in sit qui. Officia cupidatat fugiat aliquip laborum magna do deserunt. In mollit adipisicing nostrud cupidatat.\r\n",
+    "registered": "2016-06-01T02:48:50 -03:00",
+    "latitude": 7.202836,
+    "longitude": -138.648335,
+    "tags": [
+      "in",
+      "voluptate",
+      "incididunt",
+      "officia",
+      "ipsum",
+      "quis",
+      "est"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Aguirre Hurst"
+      },
+      {
+        "id": 1,
+        "name": "Daniel Munoz"
+      },
+      {
+        "id": 2,
+        "name": "Allyson Hurley"
+      }
+    ],
+    "greeting": "Hello, Teresa Hebert! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "6655b7cb4eaedbb74cf89d84",
+    "index": 5,
+    "guid": "20c07a07-ec7a-411f-8ed0-aeb9bf1b11dc",
+    "isActive": true,
+    "balance": "$3,866.93",
+    "picture": "http://placehold.it/32x32",
+    "age": 25,
+    "eyeColor": "green",
+    "name": "Beverly Rollins",
+    "gender": "female",
+    "company": "MATRIXITY",
+    "email": "beverlyrollins@matrixity.com",
+    "phone": "+1 (806) 482-2735",
+    "address": "777 Colby Court, Westerville, Arkansas, 6437",
+    "about": "Nisi qui adipisicing culpa anim tempor et consequat irure laborum anim adipisicing laborum tempor ut. Labore consectetur Lorem adipisicing elit nostrud officia cupidatat. Et aliqua cupidatat reprehenderit amet dolore laboris aute anim aliqua ad sint exercitation. Sit ex proident et quis ex. Non ut est laborum magna nulla dolor occaecat voluptate laboris ut ea minim consectetur. Fugiat reprehenderit exercitation cillum aliquip proident.\r\n",
+    "registered": "2015-11-02T05:24:55 -03:00",
+    "latitude": 84.902218,
+    "longitude": -38.132357,
+    "tags": [
+      "non",
+      "occaecat",
+      "magna",
+      "aute",
+      "laborum",
+      "officia",
+      "officia"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Hurley Barnes"
+      },
+      {
+        "id": 1,
+        "name": "Farmer Hyde"
+      },
+      {
+        "id": 2,
+        "name": "Liliana Velazquez"
+      }
+    ],
+    "greeting": "Hello, Beverly Rollins! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "6655b7cb1be07623466b8ce5",
+    "index": 6,
+    "guid": "b7184642-09a7-4c9d-9fbd-972239804072",
+    "isActive": false,
+    "balance": "$1,929.85",
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "green",
+    "name": "Blanche Howe",
+    "gender": "female",
+    "company": "NITRACYR",
+    "email": "blanchehowe@nitracyr.com",
+    "phone": "+1 (963) 463-2364",
+    "address": "257 Seabring Street, Caroline, New Jersey, 753",
+    "about": "Exercitation ullamco labore eu labore commodo ad. Id ipsum voluptate eu labore minim adipisicing veniam ut. Aliqua voluptate aliqua ad duis magna non deserunt sit ullamco eu incididunt. Et excepteur proident et sit eu aliqua officia amet.\r\n",
+    "registered": "2021-05-23T10:51:13 -03:00",
+    "latitude": 15.207206,
+    "longitude": -32.860568,
+    "tags": [
+      "sunt",
+      "sit",
+      "esse",
+      "dolore",
+      "mollit",
+      "commodo",
+      "do"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Amelia Stewart"
+      },
+      {
+        "id": 1,
+        "name": "Farrell Farmer"
+      },
+      {
+        "id": 2,
+        "name": "Cantrell Mcconnell"
+      }
+    ],
+    "greeting": "Hello, Blanche Howe! You have 4 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
